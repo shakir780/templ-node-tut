579619107aa51e1f284be256ef79e1c57e64c9ac
Shakir Kamal <shakirmadridsta1000@gmail.com>
2022-05-01 17:04:04 +0200

    first

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+const amount=8
+
+if(amount < 10)
+{
+    console.log('small number');
+}
+else{
+    console.log('large amount')
+}
+
+console.log(`hey its my first node app!!`);
---
+++ 2-global.js
@@ -0,0 +1,6 @@
+//!Globals -No WinDow
+//!__dir name -path to current directory
+//! __ filename -filename
+//!require -fuction to use modules (COMMONJS)
+//! module -info about current module (file)
+//! process -info about env where the program is being executed
